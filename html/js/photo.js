181a0d78defd66e6653a3adbe1fbc7e60fda646b
lvfeiyang <lxm19900207@163.com>
2017-11-13 10:59:47 +0800

    up

--- html/js/photo.js
+++ html/js/photo.js
@@ -120,8 +120,6 @@ $(function() {
 		height: window.innerHeight,
 		roundLengths: true,
 	});
-
-	autoPlayMusic();
 })
 
 function putSave() {
@@ -179,32 +177,4 @@ function savePhoto(photoId) {
 				window.location.reload();
 		}
 	});
-}
-// 音乐播放
-function autoPlayMusic() {
-	// 自动播放音乐效果，解决浏览器或者APP自动播放问题
-	function musicInBrowserHandler() {
-		musicPlay(true);
-		document.body.removeEventListener('touchstart', musicInBrowserHandler);
-	}
-	document.body.addEventListener('touchstart', musicInBrowserHandler);
-
-	// 自动播放音乐效果，解决微信自动播放问题
-	function musicInWeixinHandler() {
-		musicPlay(true);
-		document.addEventListener("WeixinJSBridgeReady", function () {
-			musicPlay(true);
-		}, false);
-		document.removeEventListener('DOMContentLoaded', musicInWeixinHandler);
-	}
-	document.addEventListener('DOMContentLoaded', musicInWeixinHandler);
-}
-function musicPlay(isPlay) {
-	var audio = document.getElementById('musicid');
-	if (isPlay && audio.paused) {
-		audio.play();
-	}
-	if (!isPlay && !audio.paused) {
-		audio.pause();
-	}
 }
